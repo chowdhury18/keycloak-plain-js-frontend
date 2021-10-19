4d09011633d59ddb9dda8d1544f6e6ed187063a3
arnab.chowdhury <arnab.chowdhury@dreambroker.fi>
2021-10-19 17:12:54 +0300

    keycloak plain js PKCE authorization code flow

---
+++ src/main/java/com/example/keycloakplainjs/KeycloakPlainJsApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloakplainjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakPlainJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakPlainJsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keycloakplainjs/KeycloakPlainJsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakplainjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakPlainJsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
